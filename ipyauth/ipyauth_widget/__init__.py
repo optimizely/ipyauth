d36a5485c84b64ea155cbcd9320bbb4731652f60
Mandy Trinh <mandy.trinh@optimizely.com>
2019-12-06 10:59:53 -0800

    feat: Reference ParamsOptimizely class in init file

--- ipyauth/ipyauth_widget/__init__.py
+++ ipyauth/ipyauth_widget/__init__.py
@@ -6,7 +6,7 @@ from ._widget_box import Auth
 from ._params_auth0 import ParamsAuth0
 from ._params_google import ParamsGoogle
 from ._params_sgconnect import ParamsSgConnect
-
+from ._params_optimizely import ParamsOptimizely
 
 def _jupyter_nbextension_paths():
     return [{
